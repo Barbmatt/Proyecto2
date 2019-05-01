38b815a0a745ef169d3e63d9522c1a111c0409c4
Bárbara Mattioli <barbaramattioli@outlook.com>
2019-05-01 19:32:48 -0300

    clase luz arreglada

--- Ligth.js
+++ Ligth.js
@@ -1,22 +1,16 @@
 class Ligth{
     constructor(){
-<<<<<<< HEAD
-    this.posL[3] = [10.0,10.0,10.0];
+    this.posL = [10.0,10.0,10.0];
     this.angulo;
-    this.intensidad_ambiente[3] = [1.0,1.0,1.0];
-    this.intensidad_difusa[3] = [1.0,1.0,1.0];
-=======
-    this.posL[3] = 10.0,10.0,10.0;
-    this.angulo;
-    this.iambiente[3] = 1.0,1.0,1.0;
-    this.idifusa[3] = 1.0,1.0,1.0;
->>>>>>> master
+    this.intensidad_ambiente = [1.0,1.0,1.0];
+    this.intensidad_difusa = [1.0,1.0,1.0];
     this.atenuacion;
     }
 
     func_atenuacion(a,b,c,d){
-    this.atenuacion = 1/(a+b*d+c*pow(d,2));
+    this.atenuacion = 1/(a+b*d+c*Math.pow(d,2));
     return this.atenuacion;
     }
 }
+
 
